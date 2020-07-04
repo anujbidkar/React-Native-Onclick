6670f6aa0b0cb4ebf5f43312bf8c486abc6f65a2
anujbidkar <anujbidkar8@gmail.com>
2020-07-05 01:30:50 +0530

    onclick of button and onclick of View

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
 import React from "react";
-import {View,Text,StyleSheet} from "react-native";
-import Name from './component/Name';
+import {View,StyleSheet,TextInput,Text,Button,Alert,TouchableOpacity} from "react-native";
 export default class App extends React.Component{
+  myButtonPressed(){
+    Alert.alert("text clicked");
+  }
   
+  constructor(props){
+    super(props);
+    this.state = {
+      text:""
+    };
+
+  }
   render(){
 
     return( 
     <View style={styles.container}>
-     <Name name="anuj"/>
-     <Name name="Bidkar"/>
+      <TextInput style={styles.textAnuj}
+      placeholderTextColor="black"
+      placeholder="Enter Your Name"
+      onChangeText={(text)=>{this.setState({text})}}
+      ></TextInput>
+      <Text style={styles.textnew}>{this.state.text}</Text>
+      <Button
+      title="Click"
+      onPress={()=>{
+        Alert.alert("Hello");
+      }}
+      />
 
+      <TouchableOpacity
+      onPress={this.myButtonPressed}
+      >
+        <Text style={styles.touch}>Anuj</Text>
+      </TouchableOpacity>
     </View>
     );
 
@@ -22,10 +46,28 @@ export default class App extends React.Component{
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:"pink",
+    fontSize:60,
     alignItems:"center",
-    justifyContent:"center",
-    fontSize:60
   },
-  
+  textAnuj:{
+    marginTop:100,
+    color:"red",
+    height:50,
+    width:200,
+    borderWidth:2,
+    padding:10,
+    borderRadius:15,
+    borderColor:"#23abab"
+  },
+  textnew:{
+    marginTop:5
+  },
+  touch:
+  {
+    backgroundColor:"orange",
+    padding:10,
+    borderRadius:10,
+    
+
+  }
 })
